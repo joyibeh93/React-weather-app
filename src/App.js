63ece5512f7475bfac47996a023199160bb29b50
JOYCE <joychinwenduibeh93@gmail.com>
2023-04-26 18:56:31 +0100

    added the github and netlify link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <Weather/>
         <Circlle/>
         < a href="https://github.com/joyibeh93/React-weather-app" target='_blank'>Source-Code</a>
+        <a href='https://stalwart-praline-fae7d1.netlify.app'>Hosted site on netlify</a>
     </div>
   )
 }
